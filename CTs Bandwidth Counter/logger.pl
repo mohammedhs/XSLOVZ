02dc5181dcb4fe593775f4854f33a63bf50771b0
Mohammed H <hussein.m@xsl.tel>
2015-02-25 18:15:25 +0300

    GPL License

--- CTs Bandwidth Counter/logger.pl
+++ CTs Bandwidth Counter/logger.pl
@@ -1,9 +1,20 @@
 #!/usr/bin/perl
+#	OpenVZ Bandwidth Counter through iptables
+#	Copyright (C) 2015  Mohammed H (hussein.m@xsl.tel)
 #
-#	OpenVZ Bandwidth Counter
-#	By Mohammed H @ XSLTel Hosting Services
-#	
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
 #
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 $ENV{PATH}='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin';
 use Socket;
 sub IsValidIP4{
--- WHMCS Module/XSLOVZ/XSLOVZ.php
+++ WHMCS Module/XSLOVZ/XSLOVZ.php
@@ -1,7 +1,20 @@
 <?php
 /*
-	XSLOVZ By Mohammed H (hussein.m@xsl.tel)
-	WHMCS Module to control OpenVZ Node over php ssh2 extension
+	XSLOVZ is WHMCS Module to control OpenVZ nodes over php ssh2 extension
+	Copyright (C) 2015  Mohammed H (hussein.m@xsl.tel)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 function XSLOVZ_ConfigOptions() {
 
