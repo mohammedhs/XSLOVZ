315475cbfd2fe024e34ff4e67d7ece38e5585469
Mohammed H <hussein.m@xsl.tel>
2015-03-01 04:57:10 +0300

    v1.1
    - Bugfixes .
    - Ability to limit traffic per package/per container .
    - Better way to remove container IPs .

--- WHMCS Module/XSLOVZ/XSLOVZ.php
+++ WHMCS Module/XSLOVZ/XSLOVZ.php
@@ -29,7 +29,8 @@ function XSLOVZ_ConfigOptions() {
 	 "CPU Cores" => array( "Type" => "text", "Size" => "5", "Description" => "" ),
 	 "Sudo Path" => array( "Type" => "text", "Size" => "15", "Description" => "Sudo path incase its changed", "Default" => '/usr/bin/sudo' ),
 	 "vzctl Path" => array( "Type" => "text", "Size" => "15", "Description" => "vzctl path incase its changed", "Default" => '/usr/sbin/vzctl' ),
-
+	 "Incoming Traffic" => array( "Type" => "text", "Size" => "5", "Description" => "mbit", "Default" => '100' ),
+	 "Outgoing Traffic" => array( "Type" => "text", "Size" => "5", "Description" => "mbit", "Default" => '100' ),
 	);
 	
 	return $configarray;
@@ -49,6 +50,99 @@ function XSLOVZ_getSSHPort($port,$params) {
 		}
 	return $sshport;
 	}
+function XSLOVZ_getMainInterface($if,$params) {
+	$serveraccesshash = $params["serveraccesshash"];
+	$mainif = XSLOVZ_getTextBetweenTags($serveraccesshash, $if);
+	if(empty($mainif)) {
+		$mainif = 'eth0';
+		}
+	return $mainif;
+	}
+
+function XSLOVZ_LimitTraffic($params) {
+    $ctid = intval($params["serviceid"]);
+	$sudo = $params["configoption7"];
+	$vzctl = $params["configoption8"];
+	$incomingtraffic = intval($params["configoptions"]['Incoming Traffic']);
+	if(empty($incomingtraffic)) {
+		$incomingtraffic = intval($params["configoption9"]);
+		}
+	$outgoingtraffic = intval($params["configoptions"]['Outgoing Traffic']);
+	if(empty($outgoingtraffic)) {
+		$outgoingtraffic = intval($params["configoption9"]);
+		}
+	$mainInterface = XSLOVZ_getMainInterface('interface',$params);
+    $server = $params["server"];
+    $serverip = $params["serverip"];
+    $serverusername = $params["serverusername"];
+    $serverpassword = $params["serverpassword"];
+	
+	$getctdetails = select_query("tblhosting","",array("id"=>$params['serviceid']));
+	$ctdetails = mysql_fetch_assoc($getctdetails);
+	$ctassigned_ips = $ctdetails['assignedips'];
+	$ctassigned_ips = preg_split('/\n|\r/', $ctassigned_ips, -1, PREG_SPLIT_NO_EMPTY);
+	$ctassigned_ips[] = $ctdetails["dedicatedip"];
+	$connection = ssh2_connect("$serverip", XSLOVZ_getSSHPort('port',$params));
+	ssh2_auth_password($connection, "$serverusername", "$serverpassword");
+
+	foreach($ctassigned_ips as $ip) {
+		if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+			$incomingfilter = "{$sudo} tc filter add dev venet0 protocol ipv6 parent 1:0 prio {$ctid} u32 match ipv6 dst {$ip} flowid 1:{$incomingtraffic}";
+			$outgoingfilter = "{$sudo} tc filter add dev {$mainInterface} protocol ipv6 parent 1:0 prio {$ctid} u32 match ip6 dst {$ip} flowid 1:{$outgoingtraffic}
+";
+		} else {
+			$incomingfilter = "{$sudo} tc filter add dev venet0 protocol ip parent 1:0 prio {$ctid} u32 match ip dst {$ip} flowid 1:{$incomingtraffic}";
+			$outgoingfilter = "{$sudo} tc filter add dev {$mainInterface} protocol ip parent 1:0 prio {$ctid} u32 match ip dst {$ip} flowid 1:{$outgoingtraffic}";
+		}
+		$limittraffic = ssh2_exec($connection,"
+		{$sudo} tc qdisc add dev venet0 root handle 1: htb
+		{$sudo} tc class add dev venet0 parent 1: classid 1:{$incomingtraffic} htb rate {$incomingtraffic}mbit
+		{$sudo} tc qdisc add dev venet0 parent 1:{$incomingtraffic} handle {$incomingtraffic}: sfq perturb 10
+		{$incomingfilter}
+		
+		
+		{$sudo} tc qdisc add dev {$mainInterface} root handle 1: htb
+		{$sudo} tc class add dev {$mainInterface} parent 1: classid 1:{$outgoingtraffic} htb rate {$outgoingtraffic}mbit
+		{$sudo} tc qdisc add dev {$mainInterface} parent 1:{$outgoingtraffic} handle {$outgoingtraffic}: sfq perturb 10
+		{$outgoingfilter}
+	
+		");
+		}
+    if ($limittraffic) {
+		$result = "success";
+	} else {
+		$limittraffic = "Can't Limit Traffic";
+	}
+	return $result;
+
+}
+
+function XSLOVZ_UnlimitTraffic($params) {
+    $ctid = intval($params["serviceid"]);
+	$sudo = $params["configoption7"];
+	$vzctl = $params["configoption8"];
+	$mainInterface = XSLOVZ_getMainInterface('interface',$params);
+    $server = $params["server"];
+    $serverip = $params["serverip"];
+    $serverusername = $params["serverusername"];
+    $serverpassword = $params["serverpassword"];
+	
+	$connection = ssh2_connect("$serverip", XSLOVZ_getSSHPort('port',$params));
+	ssh2_auth_password($connection, "$serverusername", "$serverpassword");
+
+	$unlimittraffic = ssh2_exec($connection,"
+		{$sudo} tc filter del dev venet0 prio {$ctid}		
+		{$sudo} tc filter del dev {$mainInterface} prio {$ctid}	
+	");
+
+    if ($limittraffic) {
+		$result = "success";
+	} else {
+		$limittraffic = "Can't Limit Traffic";
+	}
+	return $result;
+
+}
 		
 function XSLOVZ_CreateAccount($params) {
     # ** The variables listed below are passed into all module functions **
@@ -102,7 +196,7 @@ function XSLOVZ_CreateAccount($params) {
 	$assignedipv6 = XSLOVZ_getTextBetweenTags($data["assignedips"], "ipv6");
 	$assignedipv6 = preg_split('/\n|\r/', $assignedipv6, -1, PREG_SPLIT_NO_EMPTY);
 	foreach ($assignedipv6 as $ipv6) {
-	$assignedipv6 = ssh2_exec($connection,"{$sudo} {$vzctl} -a -o ip -H| grep -o -w $ipv6");
+	$assignedipv6 = ssh2_exec($connection,"{$sudo} /usr/sbin/vzlist -a -o ip -H| grep -o -w $ipv6");
 	stream_set_blocking($assignedipv6, true);
 	$assignedipv6 = trim(stream_get_contents($assignedipv6));
 	$ipv6 = trim($ipv6);
@@ -127,6 +221,8 @@ function XSLOVZ_CreateAccount($params) {
 	{$sudo} {$vzctl} set $ctid --ipadd {$assignedipv6} --save;
 	");
 	if ($createvps) {
+		XSLOVZ_UnlimitTraffic($params);
+		XSLOVZ_LimitTraffic($params);
 		$result = "success";
 	} else {
 		$result = "Can't Create Container...";
@@ -151,7 +247,7 @@ function XSLOVZ_TerminateAccount($params) {
     $serverip = $params["serverip"];
     $serverusername = $params["serverusername"];
     $serverpassword = $params["serverpassword"];
-	
+
 	$result = select_query("tblhosting","",array("id"=>$params['serviceid']));
 	$data = mysql_fetch_assoc($result);
 	$dedicated_ip = $data["dedicatedip"];
@@ -167,6 +263,8 @@ function XSLOVZ_TerminateAccount($params) {
 	");
 	
     if ($destroy) {
+		update_query("tblhosting",array("assignedips"=>""),array("id"=>$params['serviceid']));
+		XSLOVZ_UnlimitTraffic($params);
 		$result = "success";
 	} else {
 		$result = "Can't Terminate Container";
@@ -395,22 +493,29 @@ function XSLOVZ_addip($params) {
     preg_match($pattern, $string, $matches);
     return $matches[1];
 	}
-	// get IPs and assign it to existing VPS
-	$find_server = select_query("tblhosting","",array("id"=>$params['serviceid']));
-	$data = mysql_fetch_assoc($find_server);
-	$serverid = $data["server"];
-	$assigned_ips = select_query("tblservers","",array("id"=>$serverid));
-	$data = mysql_fetch_assoc($assigned_ips);
-	$assignedips = getTextBetweenTags($data["assignedips"], "ip");
-	if (!empty($assignedips)) {
-	$assignedips = preg_split('/\n|\r/', $assignedips, -1, PREG_SPLIT_NO_EMPTY);
-	foreach ($assignedips as $ip) {
-	$assignedip = ssh2_exec($connection,"{$sudo} {$vzctl} -a -o ip -H|grep -o -w $ip");
+	// get server IPs and assign it to existing VPS
+	$getctdetails = select_query("tblhosting","",array("id"=>$params['serviceid']));
+	$ctdetails = mysql_fetch_assoc($getctdetails);
+	$serverid = $ctdetails["server"];
+	$server = select_query("tblservers","",array("id"=>$serverid));
+	$server = mysql_fetch_assoc($server);
+	$server_assigned_ips = getTextBetweenTags($server["assignedips"], "ip");
+	if (!empty($server_assigned_ips)) {
+	$server_assigned_ips = preg_split('/\n|\r/', $server_assigned_ips, -1, PREG_SPLIT_NO_EMPTY);
+	foreach ($server_assigned_ips as $server_assigned_ip) {
+	$assignedip = ssh2_exec($connection,"{$sudo} /usr/sbin/vzlist -a -o ip -H|grep -o -w $server_assigned_ip");
 	stream_set_blocking($assignedip, true);
 	$assignedip = trim(stream_get_contents($assignedip));
-	$ip = trim($ip);
+	$ip = trim($server_assigned_ip);
+	$ctassignedips = $ctdetails['assignedips'];
 	if ( $assignedip != $ip) {
 		$addip = ssh2_exec($connection,"{$sudo} {$vzctl} set {$ctid} --ipadd {$ip} --save");
+		if($addip) {
+			$ctassignedips .= "\n{$ip}\n";
+			update_query("tblhosting",array("assignedips"=>$ctassignedips),array("id"=>$params['serviceid']));
+			XSLOVZ_UnlimitTraffic($params);
+			XSLOVZ_LimitTraffic($params);
+			}
 		break;
 		}
 	}	
@@ -449,21 +554,28 @@ function XSLOVZ_addipv6($params) {
     return $matches[1];
 	}
 	// get IPs and assign it to existing VPS
-	$find_server = select_query("tblhosting","",array("id"=>$params['serviceid']));
-	$data = mysql_fetch_assoc($find_server);
-	$serverid = $data["server"];
-	$assigned_ips = select_query("tblservers","",array("id"=>$serverid));
-	$data = mysql_fetch_assoc($assigned_ips);
-	$assignedips = getTextBetweenTags($data["assignedips"], "ipv6");
-	if (!empty($assignedips)) {
-	$assignedips = preg_split('/\n|\r/', $assignedips, -1, PREG_SPLIT_NO_EMPTY);
-	foreach ($assignedips as $ip) {
-	$assignedip = ssh2_exec($connection,"{$sudo} {$vzctl} -a -o ip -H|grep -o -w $ip");
+	$getctdetails = select_query("tblhosting","",array("id"=>$params['serviceid']));
+	$ctdetails = mysql_fetch_assoc($getctdetails);
+	$serverid = $ctdetails["server"];
+	$server = select_query("tblservers","",array("id"=>$serverid));
+	$server = mysql_fetch_assoc($server);
+	$server_assigned_ips = getTextBetweenTags($server["assignedips"], "ipv6");
+	if (!empty($server_assigned_ips)) {
+	$server_assigned_ips = preg_split('/\n|\r/', $server_assigned_ips, -1, PREG_SPLIT_NO_EMPTY);
+	foreach ($server_assigned_ips as $server_assigned_ip) {
+	$assignedip = ssh2_exec($connection,"{$sudo} /usr/sbin/vzlist -a -o ip -H|grep -o -w $server_assigned_ip");
 	stream_set_blocking($assignedip, true);
 	$assignedip = trim(stream_get_contents($assignedip));
-	$ip = trim($ip);
+	$ip = trim($server_assigned_ip);
+	$ctassignedips = $ctdetails['assignedips'];
 	if ( $assignedip != $ip) {
 		$addip = ssh2_exec($connection,"{$sudo} {$vzctl} set {$ctid} --ipadd {$ip} --save");
+		if($addip) {
+			$ctassignedips .= "\n{$ip}\n";
+			update_query("tblhosting",array("assignedips"=>$ctassignedips),array("id"=>$params['serviceid']));
+			XSLOVZ_UnlimitTraffic($params);
+			XSLOVZ_LimitTraffic($params);
+		}
 		break;
 		}
 	}	
@@ -474,47 +586,6 @@ function XSLOVZ_addipv6($params) {
 		$result = "success";
 	} else {
 		$result = "Can't add IP";
-	}
-	return $result;
-
-}
-
-function XSLOVZ_delip($params) {
-
-    $ctid = intval($params["serviceid"]);
-	$sudo = $params["configoption7"];
-	$vzctl = $params["configoption8"];
-
-    # Additional variables if the product/service is linked to a server
-    $server = $params["server"]; # True if linked to a server
-    $serverip = $params["serverip"];
-    $serverusername = $params["serverusername"];
-    $serverpassword = $params["serverpassword"];
-	
-	$connection = ssh2_connect("$serverip", XSLOVZ_getSSHPort('port',$params));
-	ssh2_auth_password($connection, "$serverusername", "$serverpassword");
-	// Get text from html tags
-	function getTextBetweenTags($string, $tagname) {
-    $pattern = "/\{$tagname ?.*}(.*){\/$tagname}/s";
-    preg_match($pattern, $string, $matches);
-    return $matches[1];
-	}
-	
-	$result = select_query("tblhosting","",array("id"=>$params['serviceid']));
-	$data = mysql_fetch_assoc($result);
-	$assignedips = $data["assignedips"];
-	if(!empty($assignedips)) {
-
-	$assignedips = preg_split('/\n|\r/', $assignedips, -1, PREG_SPLIT_NO_EMPTY);
-	foreach ($assignedips as $ip) {
-	$delip = ssh2_exec($connection,"{$sudo} {$vzctl} set {$ctid} --ipdel {$ip} --save;");
-	sleep(10);
-	}
-	}
-    if ($delip) {
-		$result = "success";
-	} else {
-		$result = "Can't delete IP";
 	}
 	return $result;
 
@@ -567,7 +638,7 @@ function XSLOVZ_enableppp($params) {
     if ($ppp) {
 		$result = "success";
 	} else {
-		$result = "Can't change hostname";
+		$result = "Can't enable ppp";
 	}
 	return $result;
 
@@ -594,7 +665,7 @@ function XSLOVZ_disableppp($params) {
     if ($ppp) {
 		$result = "success";
 	} else {
-		$result = "Can't change hostname";
+		$result = "Can't disable ppp";
 	}
 	return $result;
 
@@ -687,11 +758,12 @@ function XSLOVZ_AdminCustomButtonArray() {
 	 "Boot Server" => "boot",
 	 "Assign IP(s)" => "addip",
 	 "Assign IPv6" => "addipv6",
-	 "Delete IP(s)" => "delip",
 	 "Change Hostname" => "changehostname",
 	 "Apply TUN/TAP" => "tuntap",
 	 "Enable PPP" => "enableppp",
 	 "Reinstall" => "reinstall",
+	 "Limit Traffic" => "LimitTraffic",
+	 "Unlimit Traffic" => "UnlimitTraffic",
 	);
 	return $buttonarray;
 }
@@ -720,15 +792,15 @@ function XSLOVZ_UsageUpdate($params) {
 		$bandwidth = ssh2_exec($connection,"{$sudo} cat /var/lib/traffic/{$row['id']}.traffic");
 		stream_set_blocking($bandwidth, true);
 		$bandwidth = stream_get_contents($bandwidth);
-		$bandwidth = round($bandwidth/1024/1024,2);
+		$bandwidth = round($bandwidth/1000/1000,2);
 		$package = select_query("tblproducts","",array("id"=>$row['packageid']));
 		$package = mysql_fetch_assoc($package);
 		$disklimit = $package['configoption2']*1024;
 			
-		$additional_bandwidth = $params['configoptions']['Bandwidth'] * 1024;
+		$additional_bandwidth = $params['configoptions']['Bandwidth'] * 1000;
 		
 		
-		$bwlimit = $package['configoption3']*1024 + $additional_bandwidth;
+		$bwlimit = $package['configoption3']*1000 + $additional_bandwidth;
 		update_query("tblhosting",array(
          "diskusage"=>"$diskspace",
 		 "disklimit"=>"$disklimit",
@@ -738,8 +810,8 @@ function XSLOVZ_UsageUpdate($params) {
         ),array("server"=>$serverid,"id"=>$row['id']));
 		
 		$bandwidth_percent = round($bandwidth / $bwlimit * 100, 2);
-		$bandwidth_gb = round($bandwidth / 1024,2);
-		$bwlimit_gb = $bwlimit / 1024;
+		$bandwidth_gb = round($bandwidth / 1000,2);
+		$bwlimit_gb = $bwlimit / 1000;
 		
 		// Send Bandwidth Notification on 85% of usage
 		if ($row['domainstatus'] == 'Active') {
@@ -763,18 +835,42 @@ function XSLOVZ_AdminServicesTabFields($params) {
     $serverpassword = $params["serverpassword"];
 	$sudo = $params["configoption7"];
 	$vzctl = $params["configoption8"];
-	
+	$getctdetails = select_query("tblhosting","",array("id"=>$params['serviceid']));
+	$ctdetails = mysql_fetch_assoc($getctdetails);
+	$ctassignedips = $ctdetails['assignedips'];
+
 	$connection = ssh2_connect("$serverip", XSLOVZ_getSSHPort('port',$params));
 	ssh2_auth_password($connection, "$serverusername", "$serverpassword");
+	if(isset($_GET['delip'])) {
+		$ip = escapeshellarg($_GET['delip']);
+		$delip = ssh2_exec($connection,"{$sudo} {$vzctl} set $ctid --ipdel {$ip} --save");
+		if($delip) {
+			$ip = $_GET['delip'];
+			$ctassignedips = preg_replace("/{$ip}\n/",'',$ctassignedips);
+			update_query("tblhosting",array("assignedips"=>$ctassignedips),array("id"=>$params['serviceid']));
+			XSLOVZ_UnlimitTraffic($params);
+			XSLOVZ_LimitTraffic($params);
+		}
+	}
 	$current_assigned_ips = ssh2_exec($connection,"{$sudo} /usr/sbin/vzlist $ctid -o ip| grep -v IP_ADDR;");
 	stream_set_blocking($current_assigned_ips, true);
+	$ips = explode(' ',stream_get_contents($current_assigned_ips));
+	$iphtml = "";
+	foreach($ips as $ip) {
+		if(empty($ip)) {
+			break;
+			}
+		$iphtml .= "$ip <a href='clientsservices.php?id={$ctid}&delip={$ip}' >X</a> ";
+		}
+
+
 	$current_load_average = ssh2_exec($connection,"{$sudo} /usr/sbin/vzlist $ctid -o laverage| grep -v LAVERAGE;");
 	stream_set_blocking($current_load_average, true);
 	$status = ssh2_exec($connection,"{$sudo} /usr/sbin/vzlist $ctid -o status| grep -v STATUS;");
 	stream_set_blocking($status, true);
 	
     $fieldsarray = array(
-     'Current assigned IPs' => stream_get_contents($current_assigned_ips),
+     'Current assigned IPs' => $iphtml,
      'Current Load Averages' => stream_get_contents($current_load_average),
      'Status' => stream_get_contents($status),
     );
